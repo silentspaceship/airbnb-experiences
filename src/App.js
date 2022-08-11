2dfb872ecc03e0ee0dd8de1b802e65d53b6d1e93
Lucas <lucasdearaujo@pm.me>
2022-08-11 06:28:56 -0300

    content placement fixed

--- src/App.js
+++ src/App.js
@@ -6,18 +6,15 @@ import Navbar from "./components/Navbar";
 import data from "./data";
 
 function App() {
-  // const cards = data.map((item) => {
-  //   return <Card key={item.id} item={item} />;
-  // });
-
   return (
     <>
       <Navbar />
       <Hero />
-      {/* <section className="cards-container">{cards}</section> */}
-      {data.map((item) => (
-        <Card key={item.id} item={item} />
-      ))}
+      <section className="cards-container">
+        {data.map((item) => (
+          <Card key={item.id} item={item} />
+        ))}
+      </section>
     </>
   );
 }
